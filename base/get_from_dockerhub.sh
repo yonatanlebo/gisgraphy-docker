7f4320d568e1b267ceb7ed6d2a5516beb060cff0
yonatanlebo <yonatanlebo@gmail.com>
2019-11-28 16:01:59 +0200

    Update get_from_dockerhub.sh

--- base/get_from_dockerhub.sh
+++ base/get_from_dockerhub.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
-docker pull gisgraphy/gisgraphyofficial  
-docker tag  gisgraphy/gisgraphyofficial gisgraphyofficial
+docker pull search.ooguy.com:5000/gisgraphyofficial  
+docker tag  search.ooguy.com:5000/gisgraphyofficial gisgraphyofficial
